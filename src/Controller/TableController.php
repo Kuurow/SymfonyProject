e3391e9ef27898cec6c975fe1c6cad94bc2b0383
Kuurow <kuuroww@gmail.com>
2020-09-30 17:17:32 +0200

    [CORE] Number Form

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TableChoiceType;
 
 class TableController extends AbstractController
 {
@@ -17,12 +19,36 @@ class TableController extends AbstractController
         ]);
     }
     /**
-     * @Route("/table/print", name="print")
+     * @Route("/print", name="table_print")
      */
-    public function print()
+    public function print(Request $req)
     {   
+        // dump($req);
+        $num = $req->get("n");
+
         return $this->render('table/print.html.twig', [
-            '' => "",
+            'n' => $num,
         ]);
+    }
+    /**
+     * @Route("/select", name="select")
+     */
+    public function select(Request $req) 
+    {
+        $form = $this->createForm(TableChoiceType::class);
+        $form->handleRequest($req); // on demande d'analyser la requête
+
+        if ($form->isSubmitted()) { // si le formulaire est envoyé
+            $data = $form->getData();
+            $ret['n'] = $data['table_number'];
+            $response = $this->redirectToRoute('table_print', $ret);
+
+        } 
+        else {  // si le formulaire n'est pas envoyé
+            $response = $this->render('table/select.html.twig', [
+                'formulaire' => $form->createView(),
+            ]);
+        }
+        return $response;
     }
 }
